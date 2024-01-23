6026cda5129fae8f641c4f27f1b51c47a27420e7
Mirlan Boroshilov <mika.borosh@gmail.com>
2024-01-22 22:54:01 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Employee from './Components/Employee';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
+  const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState(
+    [
+      {name: 'Caleb', role: 'Developer', img: 'https://assets.entrepreneur.com/content/3x2/2000/1624449452-MohammadMatinHosseinabadiImage.jpg?format=pjeg&auto=webp&crop=4:3'},
+      {name: 'Kayla', role: 'Manager', img: 'https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg'},
+      {name: 'Mia', role: 'Intern', img: 'https://images.pexels.com/photos/3349122/pexels-photo-3349122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
+      {name: 'Nguyen', role: 'Intern', img: 'https://images.pexels.com/photos/3690702/pexels-photo-3690702.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
+      {name: 'Charlene', role: 'Intern', img: 'https://images.pexels.com/photos/3653167/pexels-photo-3653167.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
+      {name: 'Lisa', role: 'Intern', img: 'https://images.pexels.com/photos/3348748/pexels-photo-3348748.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
+    ]
+  );
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployees ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value)
+            }}
+          />
+          <div className='flex flex-wrap justify-center'>
+            {employees.map((employee) => {
+              return (<Employee key={uuidv4()} name={employee.name} role={employee.role} img={employee.img} />)
+            })}
+          </div>
+        </>
+      ) : (
+        <p>You cannot see employees</p>
+      )}
     </div>
   );
 }
